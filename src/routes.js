c95e9088132043ccfb359abe4da30ffd173c3229
cezar_pm <cezar_pm@hotmail.com>
2019-12-25 07:03:51 -0300

    Remove no console rule (eslint)

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const movieController = require('../src/controllers/MovieController');
 const WatchedmovieController = require('../src/controllers/WatchedMoviesController');
 const NoWatchedmovieController = require('../src/controllers/NoWatchedMoviesController');
@@ -9,6 +10,7 @@ routes.post('/movies', movieController.store);
 routes.get('/movies', movieController.index);
 routes.delete('/movies', movieController.delete);
 routes.put('/movies', movieController.update);
+
 
 routes.get('/movies/notwatched', NoWatchedmovieController.index);
 routes.get('/movies/watched', WatchedmovieController.index);
